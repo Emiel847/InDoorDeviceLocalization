355f23ff24f4305a8a6bb4d7b85df6643355502a
Lander <lander.buysse@gmail.com>
2019-05-08 14:04:43 +0200

    lorawan werkende versie
    klassediagram
    doc
    alle functies werken+ extra toegevoegd

--- lorawan/lorawanlibrary_zip_k64f/lorawanlibrary/click_4.cpp
+++ lorawan/lorawanlibrary_zip_k64f/lorawanlibrary/click_4.cpp
@@ -19,27 +19,22 @@ click_4::click_4(PinName Indicate, PinName wake, PinName tx, PinName rx, int bau
 /*
 *deconstructor
 */
-click_4::~click_4(){}
+click_4::~click_4(){  
+}
 
 /*
 * method to reset the device
 */
 void click_4::reset(){
-     //set NRST to o to reset 
+     //set RESET-pin to o 
     NRESET.write(0);
     wait(0.5);
-    //set reset back to 1
+    //set RESET-pin back to 1
     NRESET.write(1);
     wait(2);
-     //while(!NDATA_INDICATE);   test this
-     
-     
-    //this is normally not needed unless you want to reset the module and it's eeprom
-    //reset_cmd();
-    //factory_reset_cmd();
+    
     printf("\r\nDevice has been reset.\r\n");
-   
-    }
+}
     
 /*
 * method to set the baud rate 
@@ -51,10 +46,52 @@ void click_4:: init(){
 }
 
 /*
+*  method to initialize the lorawan module
+*
+* @param AppEUI         application EUI
+* @param sizeOfAppEUI   size of application EUI in bytes
+* @param AppKey         application key
+* @param sizeOfAppKey   size of application key in bytes
+*/
+void click_4::initialize(const uint8_t* AppEUI,int sizeOfAppEUI,const uint8_t* AppKey, int sizeOfAppKey){
+    //call method to reset the uart
+    reset();
+    //call method to reset the click4_module
+    reset_cmd();
+    //wait untill the reset is done
+    wait(2);
+    //call method to initialize and set the baud rate 
+    init(); 
+    //call method to write (the application EUI) to the eeprom  
+    eeprom_write_cmd(AppEUI,0x08,sizeOfAppEUI);
+    //call method to read the eeprom to check if the application EUI has been correctly written to the eeprom 
+    eeprom_read_cmd(0x08,8);
+    //call method to set the application key
+    set_app_key(AppKey, sizeOfAppKey);
+    //Call method to set the next data rate
+    set_next_dr_cmd(5);
+    //call method to join the network
+    join_cmd(1);
+    //wait untill the device has joined the network
+    while(get_activation_status_cmd()!=uint8_t(2)){wait(1);}
+}
+
+/*
+*  method to send data to TTN
+*
+* @param message        pointer to message to send (array of uint8_t)
+* @param sizeOfMessage  size of the message in bytes
+*/
+void click_4::sendData(uint8_t*message, int sizeOfMessage){
+  set_next_dr_cmd(5);
+  tx_msg_cmd(message, sizeOfMessage,0x00,5);
+}
+  
+/*
 * method to perform a module reset
 */
 void click_4::reset_cmd(){
-    printf("r\nReset Device.\r\n");
+    printf("\r\nReset Device.\r\n");
     
     //declare local variables
     uint8_t bufferToSend[4];
@@ -63,11 +100,13 @@ void click_4::reset_cmd(){
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
     
+    //data to send to module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x30;
     bufferToSend[2]=0x00;
     bufferToSend[3]=0x26;
     
+    //call methods to send to and receive from the module  
     send(bufferToSend,sizeof(bufferToSend)); 
     receive();
 }
@@ -77,6 +116,7 @@ void click_4::reset_cmd(){
 */    
 void click_4::factory_reset_cmd(){
     printf("\r\nFactory reset.\r\n");
+    
      //declare local variables
     uint8_t bufferToSend[4];
     
@@ -84,21 +124,23 @@ void click_4::factory_reset_cmd(){
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
     
+    //data to send to the module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x31;
     bufferToSend[2]=0x00;
     bufferToSend[3]=0x25;
-    
+        
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend)) ;
     receive();
- 
 }
 
 /*
-*method to get the EUI of the device: (needed for the loraWan communication)
+* method to get the EUI of the device: (needed for the loraWan communication)
 */
 void click_4::getDevEui(){
-    printf("\r\nDevEUI:\r\n"); 
+    printf("\r\nDevEUI:\r\n");
+     
     //declare local variables
     uint8_t bufferToSend[4];
     
@@ -106,11 +148,13 @@ void click_4::getDevEui(){
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
  
+    //data to send to the module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x36;
     bufferToSend[2]=0x00;
     bufferToSend[3]=0x20;
-   
+       
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend));
     receive();
 }
@@ -124,6 +168,7 @@ void click_4::getDevEui(){
 */
 void click_4::eeprom_write_cmd(const uint8_t* ptrToData ,uint8_t startAdress, int dataSize){
     printf("\r\nwrite to eeprom:\r\n"); 
+    
     //declare local variables
     uint8_t bufferToSend[5+dataSize];
     
@@ -131,15 +176,17 @@ void click_4::eeprom_write_cmd(const uint8_t* ptrToData ,uint8_t startAdress, in
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
     
+    //data to send to module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x32;
-    bufferToSend[2]=sizeof(bufferToSend);
+    bufferToSend[2]=1+dataSize;
     bufferToSend[3]=0x08;
     for (int i=4; i<4+dataSize; i++){
             bufferToSend[i]= (uint8_t) *(ptrToData+i-4);
         }
     bufferToSend[4+dataSize]=calculateCks(bufferToSend,sizeof(bufferToSend));
 
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend));
     receive();
 }
@@ -152,6 +199,7 @@ void click_4::eeprom_write_cmd(const uint8_t* ptrToData ,uint8_t startAdress, in
 */
 void click_4::eeprom_read_cmd(uint8_t startAddress, uint8_t numberOfBytes){
     printf("\r\nread from eeprom\r\n");
+    
     //declare local variables
     uint8_t bufferToSend[6];
     
@@ -159,6 +207,7 @@ void click_4::eeprom_read_cmd(uint8_t startAddress, uint8_t numberOfBytes){
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
     
+    //data to send to module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x33;
     bufferToSend[2]=0x02;
@@ -166,7 +215,7 @@ void click_4::eeprom_read_cmd(uint8_t startAddress, uint8_t numberOfBytes){
     bufferToSend[4]=numberOfBytes;
     bufferToSend[5]=calculateCks(bufferToSend,sizeof(bufferToSend));
     
-    
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend));
     receive();
 }
@@ -181,6 +230,7 @@ void click_4::eeprom_read_cmd(uint8_t startAddress, uint8_t numberOfBytes){
 */
 void click_4::set_app_key(const uint8_t* ptrToData , int dataSize){
     printf("\r\nset app key\r\n");
+    
     //declare local variables
     uint8_t bufferToSend[4+dataSize];
     
@@ -188,7 +238,7 @@ void click_4::set_app_key(const uint8_t* ptrToData , int dataSize){
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
 
-    //set dataToSend to module
+    //data to send to module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x43;
     bufferToSend[2]=0x10;
@@ -197,6 +247,7 @@ void click_4::set_app_key(const uint8_t* ptrToData , int dataSize){
         }
     bufferToSend[3+dataSize]= calculateCks(bufferToSend,sizeof(bufferToSend));
     
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend));
     receive();
 }
@@ -208,6 +259,7 @@ void click_4::set_app_key(const uint8_t* ptrToData , int dataSize){
 */
 void click_4::set_next_dr_cmd(uint8_t dataRate){
     printf("\r\nset next data rate\r\n");
+    
      //declare local variables
     uint8_t bufferToSend[5];
     
@@ -215,12 +267,14 @@ void click_4::set_next_dr_cmd(uint8_t dataRate){
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
     
+    //data to send to module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x4B;
     bufferToSend[2]=0x01;
     bufferToSend[3]=dataRate;
-    bufferToSend[4]= calculateCks(bufferToSend, sizeof(bufferToSend));
+    bufferToSend[4]=calculateCks(bufferToSend, sizeof(bufferToSend));
     
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend));
     receive();   
 }
@@ -234,6 +288,7 @@ void click_4::set_next_dr_cmd(uint8_t dataRate){
 */
 void click_4::join_cmd(uint8_t mode){
     printf("\r\njoin cmd\r\n");
+    
     //declare local variables
     uint8_t bufferToSend[5];
     
@@ -241,12 +296,14 @@ void click_4::join_cmd(uint8_t mode){
     j = 0;  
     memset(buff, 0x00, sizeof(buff));
     
+    //data to send to module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x40;
     bufferToSend[2]=0x01;
     bufferToSend[3]=mode;
     bufferToSend[4]=calculateCks(bufferToSend,sizeof(bufferToSend));
     
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend));
     receive();
 }
@@ -265,6 +322,7 @@ void click_4::join_cmd(uint8_t mode){
 */
 void click_4::tx_msg_cmd(uint8_t* msgPayload, uint8_t lengthOfPayload, uint8_t reliability, uint8_t port){
     printf("\r\nsend message\r\n");
+    
     //declare local variables
     uint8_t bufferToSend[6+lengthOfPayload];
     
@@ -272,9 +330,10 @@ void click_4::tx_msg_cmd(uint8_t* msgPayload, uint8_t lengthOfPayload, uint8_t r
     j = 0;  
     memset(buff, 0x00, sizeof(buff));  
     
+    //data to send to module
     bufferToSend[0]=0xAA;
     bufferToSend[1]=0x46;
-    bufferToSend[2]=(uint8_t)sizeof(bufferToSend);
+    bufferToSend[2]=2+lengthOfPayload;
     bufferToSend[3]=reliability;
     bufferToSend[4]=port;
     for(int i=5; i<5+lengthOfPayload;i++){
@@ -282,8 +341,34 @@ void click_4::tx_msg_cmd(uint8_t* msgPayload, uint8_t lengthOfPayload, uint8_t r
     }
     bufferToSend[5+lengthOfPayload]=calculateCks(bufferToSend,sizeof(bufferToSend));
     
+    //call methods to send to and receive from the module 
     send(bufferToSend,sizeof(bufferToSend));
     receive();
+}
+
+/*
+*   Method to get the activation status
+*/
+uint8_t click_4::get_activation_status_cmd(){
+    printf("\r\nactivation status: \r\n");
+    //declare local variables
+    uint8_t bufferToSend[4];
+    
+    //reset buffer and j
+    j = 0;  
+    memset(buff, 0x00, sizeof(buff));  
+    
+    //data to send to module
+    bufferToSend[0]=0xAA;
+    bufferToSend[1]=0x42;
+    bufferToSend[2]=0x00;
+    bufferToSend[3]=0x14;
+    
+    //call methods to send to and receive from the module 
+    send(bufferToSend,sizeof(bufferToSend));
+    receive();
+    printf("status code: %x", buff[3]);
+    return buff[3];
 }
 
 /*
@@ -295,6 +380,7 @@ void click_4::tx_msg_cmd(uint8_t* msgPayload, uint8_t lengthOfPayload, uint8_t r
 void click_4::send(uint8_t* ptrToData, int numberOfElements){
      NWAKE.write(0);
      printf("Data to send to module: \r\n");
+     
      for(int i=0; i<=numberOfElements-1; i++){
          printf("%x\r\n",*(ptrToData+i));
      }
@@ -329,6 +415,7 @@ void click_4::receive(){
 uint8_t click_4::calculateCks(uint8_t* ptrToData, int numberOfElements){
     int sum=0;
     printf("\r\nCalculateCks:\r\n");
+    
     for(int k=0; k<numberOfElements-1; k++){
         sum=sum+ *(ptrToData+k);
         printf("%2x  sum: %x\r\n",*(ptrToData+k),sum);
@@ -338,9 +425,11 @@ uint8_t click_4::calculateCks(uint8_t* ptrToData, int numberOfElements){
 }
 
 /*
-* method to end a session
+* method to clsoe the session: reset the lorawan module and set the WAKE pin high again
 */
 void click_4::close(){
     NWAKE.write(1);
     printf("End of session, NWAKE is set to 1\r\n");
+    //call method to reset the lorawan module
+    reset_cmd();
 }
